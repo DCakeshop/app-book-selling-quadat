4477fa06092d2fb11cd4485f405c23da78696cb6
CaoDuyQuang <108008991+DCakeshop@users.noreply.github.com>
2024-01-29 17:13:39 +0700

    Delete appbookselling_quadat directory

--- appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.appbookselling_quadat.repository;
-
-import com.example.appbookselling_quadat.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Customer findCustomerById(int id);
-}

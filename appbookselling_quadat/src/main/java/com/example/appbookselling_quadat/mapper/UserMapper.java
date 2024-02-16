3470b38de928726eb8ab9ddeecc5698332bf1ec5
CaoDuyQuang <108008991+DCakeshop@users.noreply.github.com>
2024-02-16 13:13:53 +0700

    Add files via upload

---
+++ appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.example.appbookselling_quadat.mapper;
+
+import com.example.appbookselling_quadat.beans.response.UserResponse;
+import com.example.appbookselling_quadat.entity.User;
+
+public class UserMapper {
+    public static UserResponse convertToResponse(User user){
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUsername(user.getUsername());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setUserTypeId(user.getUserTypeId());
+        return userResponse;
+    }
+}

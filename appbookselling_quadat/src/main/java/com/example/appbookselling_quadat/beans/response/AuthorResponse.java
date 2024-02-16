919e4a60e21e7a9001dae116f959e8097c5e1889
CaoDuyQuang <108008991+DCakeshop@users.noreply.github.com>
2024-02-16 14:50:49 +0700

    Add files via upload

---
+++ appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/beans/response/AuthorResponse.java
@@ -0,0 +1,15 @@
+package com.example.appbookselling_quadat.beans.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorResponse {
+    private int id;
+    private String name;
+}

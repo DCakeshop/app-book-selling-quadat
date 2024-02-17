b9a803ca224a0e7b537e04faba630800e13173b4
CaoDuyQuang <108008991+DCakeshop@users.noreply.github.com>
2024-02-17 14:28:34 +0700

    Add files via upload

---
+++ appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/exception/Order/OrderDuplicateExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.appbookselling_quadat.exception.Order;
+
+import com.example.appbookselling_quadat.exception.DuplicateException;
+import com.example.appbookselling_quadat.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+public class OrderDuplicateExceptionHandler {
+    @ExceptionHandler(DuplicateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDuplicateException(DuplicateException exception, WebRequest request){
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+}
---
+++ appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/exception/Payment/PaymentExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.appbookselling_quadat.exception.Payment;
+
+import com.example.appbookselling_quadat.exception.ErrorResponse;
+import com.example.appbookselling_quadat.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+public class PaymentExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(NotFoundException exception, WebRequest request){
+        return new ErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+}

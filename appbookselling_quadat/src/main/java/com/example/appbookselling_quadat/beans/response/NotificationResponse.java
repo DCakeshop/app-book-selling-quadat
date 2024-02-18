a27435b7d874e986a89e972e151a42fa5121c167
CaoDuyQuang <108008991+DCakeshop@users.noreply.github.com>
2024-02-18 08:55:39 +0700

    Add files via upload

---
+++ appbookselling_quadat/src/main/java/com/example/appbookselling_quadat/beans/response/NotificationResponse.java
@@ -0,0 +1,17 @@
+package com.example.appbookselling_quadat.beans.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationResponse {
+    private int id;
+    private int customerId;
+    private String message;
+    private boolean isRead;
+}
